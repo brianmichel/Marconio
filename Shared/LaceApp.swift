8360f0ec990184a77efac92eacfde702d59c8663
Brian Michel <brian.michel@gmail.com>
2022-01-27 09:32:50 -0500

    Initial Commit

---
+++ Shared/LaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaceApp.swift
+//  Shared
+//
+//  Created by Brian Michel on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct LaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
