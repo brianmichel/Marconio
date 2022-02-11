2da833b943d579aa1fd6cef690f93ce88ddadfd1
Brian Michel <brian.michel@gmail.com>
2022-02-10 20:27:42 -0500

    Update version file

--- Shared/Utility/MarconioVersionInformation.swift
+++ Shared/Utility/MarconioVersionInformation.swift
@@ -8,7 +8,7 @@ static let macVersionNumber: String = "1.0"
 /// The version number of the iOS application
 static let iOSVersionNumber: String = "1.0"
 /// The build number that would be set, a concatenation of the date and time.
-static let buildNumber: String = "2022.02.10161520"
+static let buildNumber: String = "2022.02.11012736"
 /// The git revision that is being built.
-static let gitRevision: String = "0a8f086"
+static let gitRevision: String = "ed3172f"
 }
