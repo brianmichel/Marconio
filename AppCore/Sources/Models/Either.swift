1fc0c6be6d263ab09fdbec8aded62083f9df961b
Brian Michel <brian.michel@gmail.com>
2022-02-17 22:32:26 -0500

    Adding docs for Either

--- AppCore/Sources/Models/Either.swift
+++ AppCore/Sources/Models/Either.swift
@@ -7,6 +7,36 @@
 
 import Foundation
 
+/**
+ An enumeration that lets you conditionally bind against a value that is either
+ a type of `Left` or a type of `Right`. Good for holding underlying type infromation
+ in an otherwise erased typed.
+
+ Example usage below for conditional binding in a switch.
+
+ ```
+ public var source: Either<Channel, Mixtape>
+
+ ...
+
+ switch source {
+    case let .left(channel):
+    ...
+    case let .right(mixtape):
+    ...
+ ```
+
+ Or you could use this in an if statement to bind the left or right to gate some logic.
+
+ ```
+ public var source: Either<Channel, Mixtape>
+ ...
+
+ if case let .left(channel) = source {
+    ...
+ }
+ ```
+ */
 public enum Either<Left: Equatable, Right: Equatable>: Equatable {
     case left(Left)
     case right(Right)
